87ff8abb2b83186304f81daff016101fab5ebef6
Alka Sah <alkasah485@gmail.com>
2024-05-08 14:43:43 -0700

    modification in part 3

--- script.mjs
+++ script.mjs
@@ -32,7 +32,7 @@
 //Part 3 : Feeling Loopy
 
 let data = 'ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26';
-console.log(data);
+//console.log(data);
 
    let cell1 = " ";
    let cell2 = " ";
@@ -67,7 +67,11 @@ for(let i = 0 ; i < data.length ; i++) {
         cell3 = " ";
         cell4 = " ";
     }
+
+    // console.log(data);
    }
+
+   console.log(data);
 
   
    

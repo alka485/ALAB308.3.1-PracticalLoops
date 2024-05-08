8fa2a0353f32dc01c19a6e0915219bff5bdb26d5
Alka Sah <alkasah485@gmail.com>
2024-05-08 04:57:10 -0700

    part 3 csv file

--- script.mjs
+++ script.mjs
@@ -13,18 +13,63 @@
 
 //Part 2 : Prime Time:
 
-let prime = 0;
-let num = 0;
+// let prime = 0;
+// let num = 0;
 
-while (num < 100){
-    let j = num++;
-    if(num % j ==0){
-        prime++;
-        break;
+// while (num < 100){
+//     let j = num++;
+//     if(num % j ==0){
+//         prime++;
+//         break;
+//     }
+
+//     if(prime === 0){
+//         console.log(num);
+//     }
+//     num++;
+// }
+
+//Part 3 : Feeling Loopy
+
+let data = 'ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26';
+console.log(data);
+
+   let cell1 = " ";
+   let cell2 = " ";
+   let cell3 = " ";
+   let cell4 = " ";
+
+for(let i = 0 ; i < data.length ; i++) {
+   const char = data[i];
+   //check for comma
+   if(char === ','){
+    //Move to next cell
+    if(cell1 === " "){
+        //cell1 is empty store data in cell1
+        cell1 = cell2;
+        cell2 = "";
+    }else if (cell2 === " "){
+        cell2 = cell3;
+        cell3 = " ";
+    }else if (cell3 === " "){
+        cell3 = cell4;
+        cell3 = " ";
     }
 
-    if(prime === 0){
-        console.log(num);
     }
-    num++;
-}
+
+    //check for new Line
+    else if (char === "/n") {
+        console.log(cell1,cell2,cell3,cell4);
+        //Reset cell data 
+        cell1 = " ";
+        cell2 = " ";
+        cell3 = " ";
+        cell4 = " ";
+    }
+   }
+
+  
+   
+    
+
